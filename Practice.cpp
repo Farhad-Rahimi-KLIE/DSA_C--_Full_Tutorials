f317347797a0386e8ba041e48cd4d2364090ec35
Farhad Rahimi Klie <farhadrahimiklie786@gmail.com>
2024-11-06 08:12:46 +0430

    added

--- Practice.cpp
+++ Practice.cpp
@@ -1,27 +1,9 @@
 #include<iostream>
 
 using namespace std;
-// Circular Singly Linked List
-
-struct Node
-{
-    int data;
-    Node *next;
-};
-
-Node *head = NULL;
-
-void InsertFirst(int n){
-    Node *newNode = new Node;
-    newNode->data = n;
-    if (head == NULL)
-    {
-        head = newNode;
-        newNode->next = head;
-    }
-}
 
 int main()
 {
+    
     return 0;
 }
