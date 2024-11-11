69f250c371ded2d6016b06a84b635185a8375e28
Farhad Rahimi Klie <farhadrahimiklie786@gmail.com>
2024-11-11 14:21:24 +0430

    Sort using array is complated

--- Practice.cpp
+++ Practice.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
+    int arr[10], n, x;
+    cout<<"Enter the size of array:";
+    cin>>n;
+    cout<<"Enter the Array element: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+            } 
+        }     
+    }   
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"This is Sorted array: "<<endl;
+        cout<<arr[i]<<endl;
+    }
     
     return 0;
 }
